6ae90db0af5cc458d87ef9d4718788e365641faf
Nicholas Hew <42649822+nicholashew515@users.noreply.github.com>
2018-08-23 11:56:22 -0500

    hack1

--- hello-world.c
+++ hello-world.c
@@ -1,1 +1,31 @@
+<<<<<<< HEAD
+/*
+ * Author: Nicholas Dao Han Hew
+ * Date: 2018/8/23
+=======
+/**
+ * Author: Nicholas
+ * Date: 2018/08/23
+>>>>>>> 0cfb1d119c4e83a92ac241dba327117f4cf810d3
+ *
+ * A simple hello world program in C
+ *
+ */
+<<<<<<< HEAD
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
   printf("Nicholas Dao Han Hew, Major : Mechanical Engineering ");
+=======
+
+ #include<stdlib.h>
+ #include<stdio.h>
+
+ int main(int argc, char **argv) {
+  printf("Nicholas Dao Han Hew, Major : Mechanical Engineering\n");
+  printf("Parth Patel, Major: computer engineering\n");
+
+>>>>>>> 0cfb1d119c4e83a92ac241dba327117f4cf810d3
+  return 0;
+}
