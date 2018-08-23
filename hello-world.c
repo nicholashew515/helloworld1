0cfb1d119c4e83a92ac241dba327117f4cf810d3
ParthPate1 <35845631+ParthPate1@users.noreply.github.com>
2018-08-23 11:51:55 -0500

    hack1

--- hello-world.c
+++ hello-world.c
@@ -1,1 +1,17 @@
-  printf("Nicholas Dao Han Hew, Major : Mechanical Engineering ");
+/**
+ * Author: Nicholas
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+
+ #include<stdlib.h>
+ #include<stdio.h>
+
+ int main(int argc, char **argv) {
+  printf("Nicholas Dao Han Hew, Major : Mechanical Engineering\n");
+  printf("Parth Patel, Major: computer engineering\n");
+
+  return 0;
+}
